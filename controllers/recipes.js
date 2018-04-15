e02a20f9f806f98b155f0b85cb831b0fd6a3eec0
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-04-15 15:30:09 -0400

    added to skeleton and controllers

---
+++ controllers/recipes.js
@@ -0,0 +1,49 @@
+const express = require("express")
+const router = express.Router()
+const Recipe = require('../models/recipe')
+
+let username = ''
+
+router.get('/new', (req.res) => {
+  res.render('new')
+})
+
+router.get('/:id', (req.res) => {
+  Recipe.findById(req.params.id)
+  .then( (recipes) => {
+    res.render('show', {recipes, user:req.params})
+  })
+  .catch(err=> console.log(err))
+})
+
+router.get('/', (req, res) => {
+  Recipe.find({})
+  .then(recipes => {
+    res.render('recipes-index', {user: req.user.local.email, recipes})
+  })
+  .catch(err => console.log(err))
+)}
+
+router.get('/edit/:id', (req, res) => {
+  Recipe.findOne({_id: req.params.id})
+  .then( (recipes) => {
+    res.render('edit', {recipes} )
+  })
+})
+
+router.put('/:id', (req, res) => {
+  Recipe.findOneAndUpdate({_id: req.params.id}, req.body, {new: true})
+  .then(recipes => {
+    res.redirect('/recipes')
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  Recipe.findOneAndRemove({_id: req.params.id})
+  .then(() => {
+    res.redirect('/recipes')
+  })
+})
+
+
+module.exports = router
