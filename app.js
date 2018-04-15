f05f459a147189c2fa05414b8918fe8e2da01fdd
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-04-15 14:32:55 -0400

    app.js set up

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const hbs = require('hbs')
+const parser = reqiore('body-parser')
+const cookieParser = require('cookie-parser')
+const usersController = require ('./controllers/users')
+const app = express()
+app.set("view engine", "hbs")
+
+app.use(cookieParser())
+
+
+
+
+
+app.use('/', usersController)
+
+app.use("/:user/recipes", recipesController)
+
+app.listen(3000, () => console.log("Running on port http://localhost:3000!"))
