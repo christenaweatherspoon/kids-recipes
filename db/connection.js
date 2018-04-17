56194630c0debbc42c85b0ff44ba1e85c60563ee
CHRISTENA WEATHERSPOON <greatdiamondmoon@gmail.com>
2018-04-16 21:11:22 -0400

    added to connection.js to deploy

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 if (process.env.NODE_ENV == "production") {
-  mongoose.connect(process.env.MLAB_URL)
+  mongoose.connect(process.env.ds013495.mlab.com:13495/db_kidsrecipes)
 } else {
   mongoose.connect("mongodb://localhost/db_kidsrecipes");
 }
